e9c2fea4e914c7c7b0f80826b2616b643838f6fe
huangxu <huangxu@novasoftware.cn>
2021-12-13 21:50:41 +0800

    集成swagger 3.0
    1. 注意 application.properties

--- src/main/java/com/example/demo1/controller/demoController.java
+++ src/main/java/com/example/demo1/controller/demoController.java
@@ -2,17 +2,26 @@ package com.example.demo1.controller;
 
 import com.example.demo1.mapper.UserMapper;
 import com.example.demo1.pojo.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @author Administrator
+ */
+@Api(tags="用户管理")
 @RestController
-public class demoController {
+@RequestMapping("/user")
+public class DemoController {
     @Autowired
     private UserMapper userMapper;
 
+    @ApiOperation("用户列表")
     @GetMapping("/hello")
     public List<User> hello() {
         List<User> users = userMapper.selectUserList();
