ee010e9b60ac2b95ed26621511f4f4efd825e283
huangxu <huangxu@novasoftware.cn>
2021-12-19 15:04:18 +0800

    添加log4日志记录

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,15 +1,20 @@
 package com.example.demo1;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan(basePackages= {"com.example.demo1.mapper"})
 @SpringBootApplication
 public class Demo1Application {
+    private static final Logger logger = LoggerFactory.getLogger(Demo1Application.class);
 
     public static void main(String[] args) {
+        logger.info("===============项目启动了===============");
         SpringApplication.run(Demo1Application.class, args);
+        logger.info("===============启动成功了===============");
     }
 
 }
--- src/main/java/com/example/demo1/controller/demoController.java
+++ src/main/java/com/example/demo1/controller/demoController.java
@@ -4,6 +4,8 @@ import com.example.demo1.mapper.UserMapper;
 import com.example.demo1.pojo.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +23,18 @@ public class DemoController {
     @Autowired
     private UserMapper userMapper;
 
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @ApiOperation("用户列表")
     @GetMapping("/hello")
     public List<User> hello() {
+        logger.debug("=====>测试日志debug级别打印<====");
+        logger.info("=====>测试日志info级别打印<=====");
+        logger.error("=====>测试日志error级别打印<====");
+        logger.warn("=====>测试日志warn级别打印<=====");
+
         List<User> users = userMapper.selectUserList();
         return users;
     }
+    
 }
